96fc2dcb704e4ddc78e8f827dda38abd3292f582
Ольга Іванюк <olgaivanyuk@MacBook-Pro-Olga.local>
2026-03-11 13:18:53 +0200

    Update dockerfile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Domain;
 use App\Policies\DomainPolicy;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Gate::policy(Domain::class, DomainPolicy::class);
     }
 }
